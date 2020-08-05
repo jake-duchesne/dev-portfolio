1aa4c0251d3a473ff368f0dc7f69ce369443c12a
jake-duchesne <jake14.d@gmail.com>
2020-08-04 17:37:32 -0700

    implemented authorization engine for portfolio items and blogs || also fixed portfolio_set_item for edit, update, destroy, show

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,9 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
 	layout 'portfolio'
+	access  all: [:show, :index, :angular], 
+          	user: {except: [:destroy, :new, :create, :update, :edit]}, 
+          	site_admin: :all
 
 	def index
 		@portfolio_items = Portfolio.all
@@ -30,12 +34,9 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
-
 	    respond_to do |format|
 	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -46,12 +47,9 @@ class PortfoliosController < ApplicationController
   	end
 
   	def show
-  		@portfolio_item = Portfolio.find(params[:id])
   	end
 
   	 def destroy
-  	 	# Perform lookup
-  	 	@portfolio_item = Portfolio.find(params[:id])
   	 	# Destroy the record
 	    @portfolio_item.destroy
 
@@ -69,6 +67,10 @@ class PortfoliosController < ApplicationController
   										  	:body, 
   										  	technologies_attributes: [:name]
   										  )
+  	end
+
+  	def set_portfolio_item
+  		@portfolio_item = Portfolio.find(params[:id])
   	end
 
 end
