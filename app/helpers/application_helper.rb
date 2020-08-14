7c20f214d188729454ddab40d0c1cbcf8f0dc2eb
jake-duchesne <jake14.d@gmail.com>
2020-08-14 14:17:48 -0700

    misc edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,14 +46,6 @@ module ApplicationHelper
   end
 
   def nav_helper style, tag_type
-# nav_links = <<NAV
-# <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
-# <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
-# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
-# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blogs</a></#{tag_type}>
-# <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolios</a></#{tag_type}>
-
-# NAV
 
     nav_links = ''
 
@@ -61,6 +53,14 @@ module ApplicationHelper
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
     nav_links.html_safe
+
+# nav_links = <<NAV
+# <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+# <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
+# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blogs</a></#{tag_type}>
+# <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolios</a></#{tag_type}>
+# NAV
   end
 
   def active? path
