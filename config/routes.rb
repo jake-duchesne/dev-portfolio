401a59d9f758bfa8c443862fa093f675f27c85d3
jake-duchesne <jake14.d@gmail.com>
2020-09-01 12:36:58 -0700

    implemented twitter call on tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 	get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 	get 'about', to: 'pages#about'
 	get 'contact', to: 'pages#contact'
+	get 'tech-news', to: 'pages#tech_news'
 	get 'angular-items', to: 'portfolios#angular'
 
 end
