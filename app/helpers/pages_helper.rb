20f5779a6711b233322a0f22f44a5f3fb1772901
jake-duchesne <jake14.d@gmail.com>
2020-09-01 18:52:52 -0700

    built twitter parser for tweet links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,30 @@
 module PagesHelper
-	def current_employment_info
+  def current_employment_info
 		("<h3>EM Currently Working For EM</h3>".html_safe) +
 		("Nikola Motor Corporation".html_safe)
-	end
+  end
+  
+  def twitter_parser tweet
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
